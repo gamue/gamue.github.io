8d8919e6438511e39a897af3d767e4323cb692ed
gamue <gamue@users.noreply.github.com>
2020-09-21 07:35:08 +0000

    deploy: 6f8d7312b474ca01329d6e2ab44e4b2cd06b0e69

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   content: ["./_site/**/*.html", "./_site/**/*.js"],
-  css: ["./_site/assets/css/*.css"]
+  css: ["./_site/assets/css/*.css"],
+  whitelistPatterns: [/mfp-.*/]
 };
