6f8d7312b474ca01329d6e2ab44e4b2cd06b0e69
Johannes Ganzenmüller <jganzenmller@ebay.com>
2020-09-21 09:19:05 +0200

    Analyse von PurgeCSS auf JS-Dateien erweitert

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  content: ["./_site/**/*.html"],
+  content: ["./_site/**/*.html", "./_site/**/*.js"],
   css: ["./_site/assets/css/*.css"],
-  whitelist: ["header-link"],
   whitelistPatterns: [/mfp-.*/]
 };
