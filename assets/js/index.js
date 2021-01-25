bb2d06d7310682ac3b36a8c6ce42625847911140
Johannes Ganzenmüller <jganzenmller@ebay.com>
2021-01-25 18:17:59 +0100

    Dropdown-Menü auch klickbar um Touch-Geräte zu unterstützen

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,26 @@
-let navButton = document.getElementById('navbarBtn');
-let navMenu = document.getElementById('navbarMenu');
-
-navButton.addEventListener('click', function(){
-  navMenu.classList.toggle('hidden');
+document.addEventListener('DOMContentLoaded', function() {
+  initBurgerMenu();
+  initNavDropdown();
 });
+
+function initBurgerMenu() {
+  let navButton = document.getElementById('navbarBtn');
+  let navMenu = document.getElementById('navbarMenu');
+
+  navButton.addEventListener('click', function () {
+    navMenu.classList.toggle('hidden');
+  });
+}
+
+function initNavDropdown() {
+  const dropdownBtns = document.querySelectorAll('.navToggle');
+  dropdownBtns.forEach(function(dropdownBtn) {
+    dropdownBtn.addEventListener('click', function(event) {
+      const targetBtn = event.target;
+      const navItem = targetBtn.closest('.navbarItem');
+      const drop = navItem.querySelector('.dropdownContent');
+      drop.classList.toggle("block");
+    });
+  });
+}
+
