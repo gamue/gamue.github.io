60cab9b0365c7269f444586aa6a9392f1296a816
Johannes Ganzenmüller <johannes.ganzenmueller@gmail.com>
2022-04-02 20:10:34 +0200

    404-Seite und Konstanten in data-Datei verschoben

---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "gamue.de: Blog von Johannes Ganzenmüller",
+  description:"Auf diesem Blog dreht sich alles rund um die Themen Reisen, Fotografie und Technik.",
+  url: "https://gamue.de",
+  authorEmail: "johannes@gamue.de",
+  buildTime: new Date(),
+  og_image: "assets/images/j_logo.jpg"
+};
