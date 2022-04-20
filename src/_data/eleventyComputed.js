e8ea1bced2d6d086772f229d41329727293dcfe0
Johannes Ganzenmüller <johannes.ganzenmueller@gmail.com>
2022-04-20 20:54:52 +0200

    SEO Logik migriert

---
+++ src/_data/eleventyComputed.js
@@ -0,0 +1,5 @@
+const seo = require("../_11ty/seo.js");
+
+module.exports = {
+  seo: seo,
+};
--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,10 @@ module.exports = {
   title: "gamue.de: Blog von Johannes Ganzenmüller",
   description: "Auf diesem Blog dreht sich alles rund um die Themen Reisen, Fotografie und Technik.",
   url: "https://gamue.de",
-  authorEmail: "johannes@gamue.de",
+  author:{
+    name: "Johannes Ganzenmüller",
+    email: "johannes@gamue.de"
+  },
   buildTime: new Date(),
   og_image: "assets/images/j_logo.jpg",
   accounts:{
