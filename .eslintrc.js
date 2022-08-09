a5d05cd3c92c2a35f4a84f0a0667aa4be876388b
Johannes Ganzenmueller <johannes.ganzenmueller@autoscout24.com>
2022-08-09 17:44:41 +0200

    ESLint eingefügt

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "curly": ['error', 'all'],
+        "quotes": ["error", "double"],
+    }
+}
